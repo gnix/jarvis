4401616a30078be811e75f7b653375643a9e7486
Gianni Gnesa (gnix) <gianni.gnesa@ptrace-security.com>
2020-10-22 17:54:40 +0200

    Fix path to the space.vim file

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -213,7 +213,7 @@ echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Installing Space vim-airline theme.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
-cp ~/.config/nvim/space.vim ~/.config/nvim/plugged/vim-airline-themes/autoload/airline/themes/space.vim
+cp ~/.config/nvim/space.vim ~/.config/nvim/autoload/plugged/vim-airline-themes/autoload/airline/themes/space.vim
 
 # echo "---------------------------------------------------------"
 # echo "$(tput setaf 2)JARVIS: Installing tmux plugin manager.$(tput sgr 0)"
