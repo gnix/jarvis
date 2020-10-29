388edb435228f0e6dd8115500a1e6344ce1ee1bb
Gianni Gnesa (gnix) <gianni.gnesa@ptrace-security.com>
2020-10-29 18:05:10 +0100

    Clean up the ubuntu_install.sh script

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -10,6 +10,7 @@ echo "$(tput setaf 2)JARVIS: Greetings. Preparing to power up and begin diagnost
 echo "---------------------------------------------------------"
 
 
+
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Installing system packages.$(tput sgr 0)"
 echo "---------------------------------------------------------"
@@ -40,123 +41,47 @@ do
 done
 
 
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Z (https://github.com/rupa/z).$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh
-chmod 775 z.sh
-sudo mv z.sh /usr/local/bin
-
-# Attention: If you change the z.sh location, you need to update the zshrc file as well.
-source /usr/local/bin/z.sh
-
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Python Neovim client.$(tput sgr 0)"
+echo "$(tput setaf 2)JARVIS: Installing Python packages.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
-pip3 install neovim
+packages=(
+  "neovim"     # Python Neovim client
+  "vim-vint"   # vim linter (vint)
+  "yapf"       # Google's YAPF linter
+  "pylint"     # PyLint
+  "jedi"       # Jedi
+)
 
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing NodeJS client API for Neovim$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-sudo npm install -g neovim
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing spaceship prompt$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing vim linter (vint)$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-pip3 install vim-vint
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Google's YAPF linter$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-pip3 install yapf
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing PyLint$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-pip3 install pylint
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Jedi$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-pip3 install jedi
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing bash language server$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-sudo npm i -g bash-language-server
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing colorls$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-sudo gem install colorls
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing system fonts.$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
-unzip Hack.zip -d ~/.fonts
-fc-cache -fv
-rm Hack.zip 
-
-
-# Create backup folder if it doesn't exist
-mkdir -p ~/.local/share/nvim/backup
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing oh-my-zsh.$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-else
+for i in "${packages[@]}"
+do
+  pip3 install $i
   echo "---------------------------------------------------------"
-  echo "$(tput setaf 2)JARVIS: oh-my-zsh already installed.$(tput sgr 0)"
+done
+
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing npm packages.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+packages=(
+  "neovim"                  # NodeJS client API for Neovim
+  "bash-language-server"    # bash language server
+  "vtop"
+)
+
+for i in "${packages[@]}"
+do
+  sudo npm install -g $i
   echo "---------------------------------------------------------"
-fi
+done
 
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing zsh-autosuggestions.$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing vtop.$(tput sgr 0)"
-echo "---------------------------------------------------------"
-
-sudo npm install -g vtop
-
-
-echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Creating backup directory at $BACKUP_DIR.$(tput sgr 0)"
+echo "$(tput setaf 2)JARVIS: Backing up old Neovim, ZSH, and tmux config files (Backup dir=$BACKUP_DIR).$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 BACKUP_DIR=$INSTALLDIR/backup
@@ -176,10 +101,75 @@ for filename in "${files[@]}"; do
     fi
 done
 
-
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Backup completed.$(tput sgr 0)"
 echo "---------------------------------------------------------"
+
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing Z (https://github.com/rupa/z).$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+wget https://raw.githubusercontent.com/rupa/z/master/z.sh
+chmod 775 z.sh
+sudo mv z.sh /usr/local/bin
+
+# Attention: If you change the z.sh location, you need to update the zshrc file as well.
+source /usr/local/bin/z.sh
+
+
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing colorls$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+sudo gem install colorls
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing system fonts.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+unzip Hack.zip -d ~/.fonts
+fc-cache -fv
+rm Hack.zip
+
+
+# Create backup folder if it doesn't exist
+mkdir -p ~/.local/share/nvim/backup
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing oh-my-zsh.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+else
+  echo "---------------------------------------------------------"
+  echo "$(tput setaf 2)JARVIS: oh-my-zsh already installed.$(tput sgr 0)"
+  echo "---------------------------------------------------------"
+fi
+
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing spaceship prompt$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing zsh-autosuggestions.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
 
 
 echo "---------------------------------------------------------"
@@ -200,6 +190,7 @@ for file in $linkables ; do
     ln -s $file $target
   fi
 done
+
 
 
 echo "---------------------------------------------------------"
@@ -235,20 +226,25 @@ nvim +PlugInstall +qall
 nvim +UpdateRemotePlugins +qall
 
 
+
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Installing Space vim-airline theme.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 cp ~/.config/nvim/space.vim ~/.config/nvim/autoload/plugged/vim-airline-themes/autoload/airline/themes/space.vim
 
-# echo "---------------------------------------------------------"
-# echo "$(tput setaf 2)JARVIS: Installing tmux plugin manager.$(tput sgr 0)"
-# echo "---------------------------------------------------------"
-# 
-# if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-#   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-#   ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-# fi
+
+
+echo "---------------------------------------------------------"
+echo "$(tput setaf 2)JARVIS: Installing tmux plugin manager.$(tput sgr 0)"
+echo "---------------------------------------------------------"
+
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+fi
+
+
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: Switching shell to zsh. You may need to logout.$(tput sgr 0)"
@@ -256,6 +252,8 @@ echo "---------------------------------------------------------"
 
 # sudo sh -c "echo $(which zsh) >> /etc/shells"
 chsh -s $(which zsh)
+
+
 
 echo "---------------------------------------------------------"
 echo "$(tput setaf 2)JARVIS: System update complete. Enjoy!$(tput sgr 0)"
