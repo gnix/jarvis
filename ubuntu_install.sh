15466bae40ce8c97a291034cda331b46817a340f
Gianni Gnesa (gnix) <gianni.gnesa@ptrace-security.com>
2020-11-02 10:47:21 +0100

    Rename Jarvis to Rever

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -6,13 +6,13 @@ ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Greetings. Preparing to power up and begin diagnostics.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Greetings. Preparing to power up and begin diagnostics.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing system packages.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing system packages.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 packages=(
@@ -43,7 +43,7 @@ done
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Python packages.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing Python packages.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 packages=(
@@ -63,7 +63,7 @@ done
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing npm packages.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing npm packages.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 packages=(
@@ -81,7 +81,7 @@ done
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Z (https://github.com/rupa/z).$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing Z (https://github.com/rupa/z).$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 wget https://raw.githubusercontent.com/rupa/z/master/z.sh
@@ -94,7 +94,7 @@ source /usr/local/bin/z.sh
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing colorls$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing colorls$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 sudo gem install colorls
@@ -102,7 +102,7 @@ sudo gem install colorls
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing system fonts.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing system fonts.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
@@ -116,7 +116,7 @@ mkdir -p ~/.local/share/nvim/backup
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing zsh-autosuggestions.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing zsh-autosuggestions.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -124,21 +124,21 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosugges
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing oh-my-zsh.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing oh-my-zsh.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 else
   echo "---------------------------------------------------------"
-  echo "$(tput setaf 2)JARVIS: oh-my-zsh already installed.$(tput sgr 0)"
+  echo "$(tput setaf 2)REVER: oh-my-zsh already installed.$(tput sgr 0)"
   echo "---------------------------------------------------------"
 fi
 
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Backing up old Neovim, ZSH, and tmux config files (Backup dir=$BACKUP_DIR).$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Backing up old Neovim, ZSH, and tmux config files (Backup dir=$BACKUP_DIR).$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 BACKUP_DIR=$INSTALLDIR/backup
@@ -148,24 +148,24 @@ files=("$HOME/.config/nvim" "$HOME/.zshrc" "$HOME/.tmux.conf")
 for filename in "${files[@]}"; do
     if [ -f $filename -o -d $filename ]; then
       echo "---------------------------------------------------------"
-      echo "$(tput setaf 2)JARVIS: Backing up $filename.$(tput sgr 0)"
+      echo "$(tput setaf 2)REVER: Backing up $filename.$(tput sgr 0)"
       echo "---------------------------------------------------------"
       mv $filename $BACKUP_DIR 2>/dev/null
 	else
       echo "---------------------------------------------------------"
-      echo -e "$(tput setaf 3)JARVIS: $filename does not exist at this location or is a symlink.$(tput sgr 0)"
+      echo -e "$(tput setaf 3)REVER: $filename does not exist at this location or is a symlink.$(tput sgr 0)"
       echo "---------------------------------------------------------"
     fi
 done
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Backup completed.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Backup completed.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Linking symlink files.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Linking symlink files.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 linkables=$( find -H "$INSTALLDIR" -maxdepth 3 -name '*.symlink' )
@@ -173,11 +173,11 @@ for file in $linkables ; do
   target="$HOME/.$( basename $file '.symlink' )"
   if [ -e $target ]; then
     echo "---------------------------------------------------------"
-    echo "$(tput setaf 3)JARVIS: ~${target#$HOME} already exists... Skipping.$(tput sgr 0)"
+    echo "$(tput setaf 3)REVER: ~${target#$HOME} already exists... Skipping.$(tput sgr 0)"
     echo "---------------------------------------------------------"
   else
     echo "---------------------------------------------------------"
-    echo "$(tput setaf 2)JARVIS: Creating symlink for $file.$(tput sgr 0)"
+    echo "$(tput setaf 2)REVER: Creating symlink for $file.$(tput sgr 0)"
     echo "---------------------------------------------------------"
     ln -s $file $target
   fi
@@ -186,7 +186,7 @@ done
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing config files.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing config files.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 if [ ! -d $HOME/.config ]; then
@@ -198,11 +198,11 @@ for config in $INSTALLDIR/config/*; do
   target=$HOME/.config/$( basename $config )
   if [ -e $target ]; then
     echo "---------------------------------------------------------"
-    echo "$(tput setaf 3)JARVIS: ~${target#$HOME} already exists... Skipping.$(tput sgr 0)"
+    echo "$(tput setaf 3)REVER: ~${target#$HOME} already exists... Skipping.$(tput sgr 0)"
     echo "---------------------------------------------------------"
   else
     echo "---------------------------------------------------------"
-    echo "$(tput setaf 2)JARVIS: Creating symlink for ${config}.$(tput sgr 0)"
+    echo "$(tput setaf 2)REVER: Creating symlink for ${config}.$(tput sgr 0)"
     echo "---------------------------------------------------------"
     ln -s $config $target
   fi
@@ -211,7 +211,7 @@ done
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Neovim plugins and linking dotfiles.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing Neovim plugins and linking dotfiles.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 nvim +PlugInstall +qall
@@ -220,7 +220,7 @@ nvim +UpdateRemotePlugins +qall
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing Space vim-airline theme.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing Space vim-airline theme.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 cp ~/.config/nvim/space.vim ~/.config/nvim/autoload/plugged/vim-airline-themes/autoload/airline/themes/space.vim
@@ -228,7 +228,7 @@ cp ~/.config/nvim/space.vim ~/.config/nvim/autoload/plugged/vim-airline-themes/a
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing spaceship prompt$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing spaceship prompt$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
@@ -237,7 +237,7 @@ ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/the
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Installing tmux plugin manager.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Installing tmux plugin manager.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
@@ -248,7 +248,7 @@ fi
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: Switching shell to zsh. You may need to logout.$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: Switching shell to zsh. You may need to logout.$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 # sudo sh -c "echo $(which zsh) >> /etc/shells"
@@ -257,7 +257,7 @@ chsh -s $(which zsh)
 
 
 echo "---------------------------------------------------------"
-echo "$(tput setaf 2)JARVIS: System update complete. Enjoy!$(tput sgr 0)"
+echo "$(tput setaf 2)REVER: System update complete. Enjoy!$(tput sgr 0)"
 echo "---------------------------------------------------------"
 
 exit 0
